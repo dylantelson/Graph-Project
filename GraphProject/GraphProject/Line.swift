60d52c65e7140f0b3e5295152f9d48647ae7a44b
Dylan Telson <dtelson1@binghamton.edu>
2019-09-14 16:19:04 -0400

    Stock Market API JSON parsing working, but cannot figure out how to make the draw function run after the data is acquired. JSON parsing help from https://www.youtube.com/watch?v=9iazQQdNoNU

--- GraphProject/GraphProject/Line.swift
+++ GraphProject/GraphProject/Line.swift
@@ -6,52 +6,60 @@
 //  Copyright © 2019 Dylan Telson. All rights reserved.
 //
 
+struct Points {
+    static var myPoints = [Float]()
+}
+
 import UIKit
 import Foundation
 
 class Line: UIView {
     
-    // used Roberto Viana's youtube video https://www.youtube.com/watch?v=9sJxtzTo8W0 to learn how to use UIBezierPath and basic methods for drawing lines
+   // var myPoints = [[CGFloat]]()
     
-    var myPoints = [[CGFloat]]()
+    // used Roberto Viana's youtube video https://www.youtube.com/watch?v=9sJxtzTo8W0 to learn how to use UIBezierPath and basic methods for drawing lines
    
     var myLine = UIBezierPath()
-    
-    func getJsonFromUrl(){
-        //creating a NSURL
-        let url = NSURL(string: "https://cloud.iexapis.com/stable/stock/aapl/chart?token=sk_5b18d8815df14e43a9e8ba4f8ab693db")
-        
-        //fetching the data from the url
-        URLSession.shared.dataTask(with: (url as URL?)!, completionHandler: {(data, response, error) -> Void in
-            
-            
-        }).resume()
-    }
     
     //TODO LIST:
     //Probably change it so only Y is taken, rather than X and Y, as X will end up just being every day for stock market and thus will can just be bounds.width/myPoints.count
     //Make JSON work, taking it from a URL, and use IEXCloud's API (https://iexcloud.io/console/tokens) to get data for each stock (Example for Apple stock of last month: https://cloud.iexapis.com/stable/stock/aapl/quote?token=sk_5b18d8815df14e43a9e8ba4f8ab693db
     //Add 7 or so companies, such as Apple Microsoft etc. and make each a different color
     //Add background which shows legend, x and y grid, etc
+    
+    func redraw() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            self.setNeedsDisplay()
+        }
+    }
 
     func graph() {
-        myPoints.append([])
-        myPoints.append([])
-        myPoints[0].append(bounds.width/2)
-        myPoints[0].append(bounds.height/2)
-        myPoints[1].append(bounds.width)
-        myPoints[1].append(bounds.height)
-        myLine.move(to: .init(x:0, y: bounds.height/2))
-        for point in myPoints {
-            myLine.addLine(to: .init(x: point[0], y: point[1]))
-        }
         
+        Points.myPoints.append(50)
+        Points.myPoints.append(150)
+        Points.myPoints.append(20)
+        Points.myPoints.append(50)
+        myLine.move(to: .init(x:0, y: Int(Points.myPoints[0])))
+        for n in 1...Points.myPoints.count-1 {
+            myLine.addLine(to: .init(x: n * 30, y: Int(Points.myPoints[n])))
+        }
+
         UIColor.red.setStroke()
         myLine.lineWidth = 2
         myLine.stroke()
     }
 
+    var test = 0
     override func draw(_ rect: CGRect) {
-        graph()
+        if(test == 0){
+            test = test + 1
+            return
+        }
+        print("wow")
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+            self.graph()
+            //        }
+        //self.graph()
     }
+}
 }
--- GraphProject/GraphProject/ViewController.swift
+++ GraphProject/GraphProject/ViewController.swift
@@ -8,11 +8,70 @@
 
 import UIKit
 
+struct Stock: Decodable {
+    let date: String
+    let open: Float
+    let close: Float
+    let high: Float
+    let low: Float
+    let volume: Float
+    let uOpen: Float
+    let uClose: Float
+    let uHigh: Float
+    let uLow: Float
+    let uVolume: Float
+    let change: Float
+    let changePercent: Float
+    let label: String
+    let changeOverTime: Float
+}
+
 class ViewController: UIViewController {
+    
+    let myLine = Line()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //JSON Parsing with help from https://www.youtube.com/watch?v=9iazQQdNoNU by Lets Build That App
+        fetchCoursesJSON { (stocks, err) in
+            if let err = err {
+                print("Failure", err)
+                return
+            }
+            
+            stocks?.forEach({ (stock) in
+                print(stock.close)
+                Points.myPoints.append(stock.close)
+            })
+            self.myLine.redraw()
+        }
+        //print(Points.myPoints.count)
+    }
+    
+    
+    fileprivate func fetchCoursesJSON(completion: @escaping ([Stock]?, Error?) ->
+        ()) {
+        
+        let urlString = "https://cloud.iexapis.com/stable/stock/aapl/chart?token=sk_5b18d8815df14e43a9e8ba4f8ab693db"
+        guard let url = URL(string: urlString) else { return }
+        
+        URLSession.shared.dataTask(with:url) { (data, response, err) in
+            
+            if let err = err {
+                completion(nil, err)
+                return
+            }
+            
+            //If successful
+            do {
+                let stocks = try JSONDecoder().decode([Stock].self, from: data!)
+                completion(stocks, nil)
+            } catch let jsonError {
+                completion(nil, jsonError)
+            }
+            
+        }.resume()
     }
 
 
