640cd466400f6f1997feb0a7664591088e4e8f9c
Dylan Telson <dtelson1@binghamton.edu>
2019-09-19 17:53:06 -0400

    Got stock API working with drawing the line graphs. Now the line graph gets the data of the past month from the API for 8 stocks and visualizes them, each with a different color. Need to add the background to show the X and Y, and mouseover data, and perhaps add some more stocks.

--- GraphProject/GraphProject/Line.swift
+++ GraphProject/GraphProject/Line.swift
@@ -7,7 +7,7 @@
 //
 
 struct Points {
-    static var myPoints = [[Float](), [Float](), [Float](), [Float](), [Float](), [Float]()]
+    static var myPoints = [[Float](), [Float](), [Float](), [Float](), [Float](), [Float](), [Float](), [Float]()]
 }
 
 import UIKit
@@ -41,6 +41,15 @@ class Line: UIView {
     // used Roberto Viana's youtube video https://www.youtube.com/watch?v=9sJxtzTo8W0 to learn how to use UIBezierPath and basic methods for drawing lines
    
     var myLine = UIBezierPath()
+    var myLine2 = UIBezierPath()
+    var myLine3 = UIBezierPath()
+    var myLine4 = UIBezierPath()
+    var myLine5 = UIBezierPath()
+    var myLine6 = UIBezierPath()
+    var myLine7 = UIBezierPath()
+    var myLine8 = UIBezierPath()
+    
+    lazy var myLines = [myLine, myLine2, myLine3, myLine4, myLine5, myLine6, myLine7, myLine8]
     
     //TODO LIST:
     //Probably change it so only Y is taken, rather than X and Y, as X will end up just being every day for stock market and thus will can just be bounds.width/myPoints.count
@@ -65,14 +74,21 @@ class Line: UIView {
         //self.frame = CGRect(x: 100 , y: 100, width: 100, height: 100)
         
         for n in 0...Points.myPoints.count-1 {
-            myLine.move(to: .init(x:0, y: Int(Points.myPoints[n][0]) - 150))
+            if(n==0) {UIColor.red.setStroke()}
+            else if(n==1) {UIColor.blue.setStroke()}
+            else if(n==2) {UIColor.green.setStroke()}
+            else if(n==3) {UIColor.white.setStroke()}
+            else if(n==4) {UIColor.brown.setStroke()}
+            else if(n==5) {UIColor.yellow.setStroke()}
+            else if(n==6) {UIColor.purple.setStroke()}
+            else if(n==7) {UIColor.cyan.setStroke()}
+            myLines[n].lineWidth = 2
+            myLines[n].move(to: .init(x:0, y: abs(Int(Points.myPoints[n][0]) - 200)))
             for m in 1...Points.myPoints[n].count - 1 {
-                myLine.addLine(to: .init(x: n * 30, y: Int(Points.myPoints[n][m]) - 150))
+                myLines[n].addLine(to: .init(x: m * 30, y: abs(Int(Points.myPoints[n][m]) - 200)))
             }
+            myLines[n].stroke()
         }
-        UIColor.red.setStroke()
-        myLine.lineWidth = 2
-        myLine.stroke()
     }
 
     override func draw(_ rect: CGRect) {
--- GraphProject/GraphProject/ViewController.swift
+++ GraphProject/GraphProject/ViewController.swift
@@ -28,92 +28,49 @@ struct Stock: Decodable {
 
 class ViewController: UIViewController {
     
-    var counterForJSON = 0
     
-    let urlList = ["https://cloud.iexapis.com/stable/stock/aapl/chart?token=sk_5b18d8815df14e43a9e8ba4f8ab693db", "https://cloud.iexapis.com/stable/stock/msft/chart?token=sk_5b18d8815df14e43a9e8ba4f8ab693db", "https://cloud.iexapis.com/stable/stock/ge/chart?token=sk_5b18d8815df14e43a9e8ba4f8ab693db", "https://cloud.iexapis.com/stable/stock/bac/chart?token=sk_5b18d8815df14e43a9e8ba4f8ab693db", "https://cloud.iexapis.com/stable/stock/ndaq/chart?token=sk_5b18d8815df14e43a9e8ba4f8ab693db", "https://cloud.iexapis.com/stable/stock/aon/chart?token=sk_5b18d8815df14e43a9e8ba4f8ab693db"]
+    var counterForJSON = 0
+    var counterForJSON2 = 0
+    
+    let urlList = ["https://cloud.iexapis.com/stable/stock/aapl/chart?token=sk_5b18d8815df14e43a9e8ba4f8ab693db", "https://cloud.iexapis.com/stable/stock/msft/chart?token=sk_5b18d8815df14e43a9e8ba4f8ab693db", "https://cloud.iexapis.com/stable/stock/ge/chart?token=sk_5b18d8815df14e43a9e8ba4f8ab693db", "https://cloud.iexapis.com/stable/stock/bac/chart?token=sk_5b18d8815df14e43a9e8ba4f8ab693db", "https://cloud.iexapis.com/stable/stock/ndaq/chart?token=sk_5b18d8815df14e43a9e8ba4f8ab693db", "https://cloud.iexapis.com/stable/stock/aon/chart?token=sk_5b18d8815df14e43a9e8ba4f8ab693db", "https://cloud.iexapis.com/stable/stock/bcyc/chart?token=sk_5b18d8815df14e43a9e8ba4f8ab693db", "https://cloud.iexapis.com/stable/stock/fb/chart?token=sk_5b18d8815df14e43a9e8ba4f8ab693db",]
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let value = UIInterfaceOrientation.landscapeLeft.rawValue
+        UIDevice.current.setValue(value, forKey: "orientation")
+        
         //JSON Parsing with help from https://www.youtube.com/watch?v=9iazQQdNoNU by Lets Build That App
-        fetchAAPLJSON { (stocks, err) in
-            if let err = err {
-                print("Failure", err)
-                return
-            }
+        while(counterForJSON < urlList.count) {
+            fetchAAPLJSON { (stocks, err) in
+                if let err = err {
+                    print("Failure", err)
+                    return
+                }
                 
-            stocks?.forEach({ (stock) in
-                print(stock.close)
-                Points.myPoints[0].append(stock.close)
-            })
-        }
-        fetchMSFTJSON { (stocks, err) in
-            if let err = err {
-                print("Failure", err)
-                return
+                stocks?.forEach({ (stock) in
+                    print(stock.close)
+                    Points.myPoints[self.counterForJSON2].append(stock.close)
+                })
             }
-            
-            stocks?.forEach({ (stock) in
-                print(stock.close)
-                Points.myPoints[1].append(stock.close)
-            })
-        }
-        fetchGEJSON { (stocks, err) in
-            if let err = err {
-                print("Failure", err)
-                return
-            }
-            
-            stocks?.forEach({ (stock) in
-                print(stock.close)
-                Points.myPoints[2].append(stock.close)
-            })
-        }
-        fetchBACJSON { (stocks, err) in
-            if let err = err {
-                print("Failure", err)
-                return
-            }
-            
-            stocks?.forEach({ (stock) in
-                print(stock.close)
-                Points.myPoints[3].append(stock.close)
-            })
-        }
-        fetchNDAQJSON { (stocks, err) in
-            if let err = err {
-                print("Failure", err)
-                return
-            }
-            
-            stocks?.forEach({ (stock) in
-                print(stock.close)
-                Points.myPoints[4].append(stock.close)
-            })
-        }
-        fetchAONJSON { (stocks, err) in
-            if let err = err {
-                print("Failure", err)
-                return
-            }
-            
-            stocks?.forEach({ (stock) in
-                print(stock.close)
-                Points.myPoints[5].append(stock.close)
-            })
+            counterForJSON+=1
         }
         
-        DispatchQueue.main.async {
-            let myLine = Line(frame: CGRect(x: UIScreen.main.bounds.width/4, y: UIScreen.main.bounds.height/4, width: UIScreen.main.bounds.width/2, height: UIScreen.main.bounds.height/2))
-            self.view.addSubview(myLine)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                let myLine = Line(frame: CGRect(x: UIScreen.main.bounds.width/4 - 25, y: UIScreen.main.bounds.height/4 - 25, width: UIScreen.main.bounds.width/2 + 50, height: UIScreen.main.bounds.height/2 + 50))
+                self.view.addSubview(myLine)
         }
     }
     
+    override var shouldAutorotate: Bool {
+        return true
+    }
     
     fileprivate func fetchAAPLJSON(completion: @escaping ([Stock]?, Error?) ->
         ()) {
         
         
-        let urlString = "https://cloud.iexapis.com/stable/stock/aapl/chart?token=sk_5b18d8815df14e43a9e8ba4f8ab693db"
+        let urlString = urlList[self.counterForJSON]
         
         guard let url = URL(string: urlString) else { return }
         
@@ -131,139 +88,139 @@ class ViewController: UIViewController {
             } catch let jsonError {
                 completion(nil, jsonError)
             }
-            
+            self.counterForJSON2 += 1
         }.resume()
     }
     
-    fileprivate func fetchMSFTJSON(completion: @escaping ([Stock]?, Error?) ->
-        ()) {
-        
-        
-        let urlString = "https://cloud.iexapis.com/stable/stock/msft/chart?token=sk_5b18d8815df14e43a9e8ba4f8ab693db"
-        
-        guard let url = URL(string: urlString) else { return }
-        
-        URLSession.shared.dataTask(with:url) { (data, response, err) in
-            
-            if let err = err {
-                completion(nil, err)
-                return
-            }
-            
-            //If successful
-            do {
-                let stocks = try JSONDecoder().decode([Stock].self, from: data!)
-                completion(stocks, nil)
-            } catch let jsonError {
-                completion(nil, jsonError)
-            }
-            
-            }.resume()
-    }
-    
-    fileprivate func fetchGEJSON(completion: @escaping ([Stock]?, Error?) ->
-        ()) {
-        
-        
-        let urlString = "https://cloud.iexapis.com/stable/stock/ge/chart?token=sk_5b18d8815df14e43a9e8ba4f8ab693db"
-        
-        guard let url = URL(string: urlString) else { return }
-        
-        URLSession.shared.dataTask(with:url) { (data, response, err) in
-            
-            if let err = err {
-                completion(nil, err)
-                return
-            }
-            
-            //If successful
-            do {
-                let stocks = try JSONDecoder().decode([Stock].self, from: data!)
-                completion(stocks, nil)
-            } catch let jsonError {
-                completion(nil, jsonError)
-            }
-            
-            }.resume()
-    }
-    
-    fileprivate func fetchBACJSON(completion: @escaping ([Stock]?, Error?) ->
-        ()) {
-        
-        
-        let urlString = "https://cloud.iexapis.com/stable/stock/bac/chart?token=sk_5b18d8815df14e43a9e8ba4f8ab693db"
-        
-        guard let url = URL(string: urlString) else { return }
-        
-        URLSession.shared.dataTask(with:url) { (data, response, err) in
-            
-            if let err = err {
-                completion(nil, err)
-                return
-            }
-            
-            //If successful
-            do {
-                let stocks = try JSONDecoder().decode([Stock].self, from: data!)
-                completion(stocks, nil)
-            } catch let jsonError {
-                completion(nil, jsonError)
-            }
-            
-            }.resume()
-    }
-    
-    fileprivate func fetchNDAQJSON(completion: @escaping ([Stock]?, Error?) ->
-        ()) {
-        
-        
-        let urlString = "https://cloud.iexapis.com/stable/stock/ndaq/chart?token=sk_5b18d8815df14e43a9e8ba4f8ab693db"
-        
-        guard let url = URL(string: urlString) else { return }
-        
-        URLSession.shared.dataTask(with:url) { (data, response, err) in
-            
-            if let err = err {
-                completion(nil, err)
-                return
-            }
-            
-            //If successful
-            do {
-                let stocks = try JSONDecoder().decode([Stock].self, from: data!)
-                completion(stocks, nil)
-            } catch let jsonError {
-                completion(nil, jsonError)
-            }
-            
-            }.resume()
-    }
-    
-    fileprivate func fetchAONJSON(completion: @escaping ([Stock]?, Error?) ->
-        ()) {
-        
-        
-        let urlString = "https://cloud.iexapis.com/stable/stock/aon/chart?token=sk_5b18d8815df14e43a9e8ba4f8ab693db"
-        
-        guard let url = URL(string: urlString) else { return }
-        
-        URLSession.shared.dataTask(with:url) { (data, response, err) in
-            
-            if let err = err {
-                completion(nil, err)
-                return
-            }
-            
-            //If successful
-            do {
-                let stocks = try JSONDecoder().decode([Stock].self, from: data!)
-                completion(stocks, nil)
-            } catch let jsonError {
-                completion(nil, jsonError)
-            }
-            
-            }.resume()
-    }
+//    fileprivate func fetchMSFTJSON(completion: @escaping ([Stock]?, Error?) ->
+//        ()) {
+//
+//
+//        let urlString2 = "https://cloud.iexapis.com/stable/stock/msft/chart?token=sk_5b18d8815df14e43a9e8ba4f8ab693db"
+//
+//        guard let url2 = URL(string: urlString2) else {
+//            return }
+//
+//        URLSession.shared.dataTask(with:url2) { (data, response, err) in
+//            if let err = err {
+//                completion(nil, err)
+//                return
+//            }
+//
+//            //If successful
+//            do {
+//                let stocks = try JSONDecoder().decode([Stock].self, from: data!)
+//                completion(stocks, nil)
+//            } catch let jsonError {
+//                completion(nil, jsonError)
+//            }
+//
+//        }.resume()
+//    }
+//
+//    fileprivate func fetchGEJSON(completion: @escaping ([Stock]?, Error?) ->
+//        ()) {
+//
+//
+//        let urlString = "https://cloud.iexapis.com/stable/stock/ge/chart?token=sk_5b18d8815df14e43a9e8ba4f8ab693db"
+//
+//        guard let url = URL(string: urlString) else { return }
+//
+//        URLSession.shared.dataTask(with:url) { (data, response, err) in
+//
+//            if let err = err {
+//                completion(nil, err)
+//                return
+//            }
+//
+//            //If successful
+//            do {
+//                let stocks = try JSONDecoder().decode([Stock].self, from: data!)
+//                completion(stocks, nil)
+//            } catch let jsonError {
+//                completion(nil, jsonError)
+//            }
+//
+//            }.resume()
+//    }
+//
+//    fileprivate func fetchBACJSON(completion: @escaping ([Stock]?, Error?) ->
+//        ()) {
+//
+//
+//        let urlString = "https://cloud.iexapis.com/stable/stock/bac/chart?token=sk_5b18d8815df14e43a9e8ba4f8ab693db"
+//
+//        guard let url = URL(string: urlString) else { return }
+//
+//        URLSession.shared.dataTask(with:url) { (data, response, err) in
+//
+//            if let err = err {
+//                completion(nil, err)
+//                return
+//            }
+//
+//            //If successful
+//            do {
+//                let stocks = try JSONDecoder().decode([Stock].self, from: data!)
+//                completion(stocks, nil)
+//            } catch let jsonError {
+//                completion(nil, jsonError)
+//            }
+//
+//            }.resume()
+//    }
+//
+//    fileprivate func fetchNDAQJSON(completion: @escaping ([Stock]?, Error?) ->
+//        ()) {
+//
+//
+//        let urlString = "https://cloud.iexapis.com/stable/stock/ndaq/chart?token=sk_5b18d8815df14e43a9e8ba4f8ab693db"
+//
+//        guard let url = URL(string: urlString) else { return }
+//
+//        URLSession.shared.dataTask(with:url) { (data, response, err) in
+//
+//            if let err = err {
+//                completion(nil, err)
+//                return
+//            }
+//
+//            //If successful
+//            do {
+//                let stocks = try JSONDecoder().decode([Stock].self, from: data!)
+//                completion(stocks, nil)
+//            } catch let jsonError {
+//                completion(nil, jsonError)
+//            }
+//
+//            }.resume()
+//    }
+//
+//    fileprivate func fetchAONJSON(completion: @escaping ([Stock]?, Error?) ->
+//        ()) {
+//
+//
+//        let urlString = "https://cloud.iexapis.com/stable/stock/aon/chart?token=sk_5b18d8815df14e43a9e8ba4f8ab693db"
+//
+//        guard let url = URL(string: urlString) else { return }
+//
+//        URLSession.shared.dataTask(with:url) { (data, response, err) in
+//
+//            if let err = err {
+//                completion(nil, err)
+//                return
+//            }
+//
+//            //If successful
+//            do {
+//                let stocks = try JSONDecoder().decode([Stock].self, from: data!)
+//                completion(stocks, nil)
+//            } catch let jsonError {
+//                completion(nil, jsonError)
+//            }
+//
+//            }.resume()
+//    }
 
 
 }
